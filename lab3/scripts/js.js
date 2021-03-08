bd76ed19af41c339bad8cde52a51a348461cf518
Vladosok96 <vmixoks@gmail.com>
2021-03-08 16:05:10 +0400

    Update js.js

--- lab3/scripts/js.js
+++ lab3/scripts/js.js
@@ -1,3 +1,20 @@
 function run() {
-	console.log(document.getElementById("input-field").value.split(/\s/))
+	let inputData = document.getElementById("input-field").value.split("\n");
+	console.log(inputData);
+
+	let arrWidth = inputData[0].split(" ").length;
+	let isVald = true;
+	for (let i = 1; i < inputData.length; i++) {
+		let splitPieceOfData = inputData[i].split(" ")
+		if (splitPieceOfData.length != arrWidth) {
+			isVald = false;
+		}
+	}
+
+	if (isVald) {
+		
+	}
+	else {
+		alert("Матрица не прямоугольная!");
+	}
 }
