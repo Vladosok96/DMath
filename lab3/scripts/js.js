481123d906f643be0be463186f90eab831a42beb
Vladosok96 <vmixoks@gmail.com>
2021-03-08 16:51:03 +0400

    summ

--- lab3/scripts/js.js
+++ lab3/scripts/js.js
@@ -19,7 +19,26 @@ function run() {
 	}
 
 	if (Vald == 0) {
-		
+		let checkY = true;
+		for (let i = 0; i < inputData.length; i++) {
+			let checkX = false;
+			let splitPieceOfData = inputData[i].split(" ");
+			for (let j = 0; j < arrWidth; j++) {
+				if (splitPieceOfData[j] == 1) {
+					checkX = true;
+				}
+			}
+
+			if (!checkX) {
+				checkY = false;
+			}
+		}
+		if (checkY) {
+			document.getElementById("result").innerHTML = "является";
+		}
+		else {
+			document.getElementById("result").innerHTML = "не является";
+		}
 	}
 	else if (Vald == 1){
 		alert("В матрице не может быть элементов, отличных от нуля или единицы!");
