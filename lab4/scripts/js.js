3a5b64c3d7fced5c57a9b705fc7826d35209adba
Vladosok96 <vmixoks@gmail.com>
2021-03-11 20:42:15 +0400

    Update js.js

--- lab4/scripts/js.js
+++ lab4/scripts/js.js
@@ -2,13 +2,24 @@ function run() {
 	let inputData = document.getElementById("input-field").value.split("\n");
 	console.log(inputData);
 
+	size = inputData.length;
+	var array = new Array(size);
+
 	let Vald = 0;
 	for (let i = 0; i < inputData.length; i++) {
 		let splitPieceOfData = inputData[i].split(" ");
 
+		array[i] = new Array(size);
+
 		for (let j = 0; j < splitPieceOfData.length; j++) {
 			if (isNaN(splitPieceOfData[j]) && splitPieceOfData[j] != "i"){
 				Vald = 1;
+			}
+			else if (i == j) {
+				array[i][j] = "0";
+			}
+			else {
+				array[i][j] = splitPieceOfData[j];
 			}
 		}
 
@@ -18,7 +29,10 @@ function run() {
 	}
 
 	if (Vald == 0) {
-		
+		for (let iter = 0; iter < inputData.length; iter++) {
+			var arrayTmp = array;
+			console.log(arrayTmp);
+		}
 	}
 	else if (Vald == 1){
 		alert("В матрице могут быть только числа либо буква \"i\"!");
