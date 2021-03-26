1633def626da2b0bd8833ef320b16c7e9f84a721
Vladosok96 <vmixoks@gmail.com>
2021-03-26 20:05:18 +0400

    summ

--- lab5/scripts/js.js
+++ lab5/scripts/js.js
@@ -1,18 +1,18 @@
 function run() {
-	let inputData = document.getElementById("input-field").value.split("\n");
+	var inputData = document.getElementById("input-field").value.split("\n");
 	console.log(inputData);
 
-	let size = inputData.length;
+	var size = inputData.length;
 	var array = new Array(size);
-	let tmpInt
+	var tmpInt
 
-	let Vald = 0;
-	for (let i = 0; i < inputData.length; i++) {
-		let splitPieceOfData = inputData[i].split(" ");
+	var Vald = 0;
+	for (var i = 0; i < inputData.length; i++) {
+		var splitPieceOfData = inputData[i].split(" ").filter(elem => elem != false);
 
 		array[i] = new Array(size);
 
-		for (let j = 0; j < splitPieceOfData.length; j++) {
+		for (var j = 0; j < splitPieceOfData.length; j++) {
 			if (isNaN(splitPieceOfData[j]) && splitPieceOfData[j] != "z"){
 				Vald = 1;
 			}
@@ -23,18 +23,18 @@ function run() {
 				array[i][j] = splitPieceOfData[j];
 			}
 		}
-
+		console.log(splitPieceOfData);
 		if (splitPieceOfData.length != inputData.length) {
 			Vald = 2;
 		}
 	}
 
 	if (Vald == 0) {
-		for (let iter = 0; iter < inputData.length; iter++) {
+		for (var iter = 0; iter < inputData.length; iter++) {
 			var arrayTmp = array;
 			
-			for (let i = 0; i < inputData.length; i++) {
-				for (let j = 0; j < inputData.length; j++) {
+			for (var i = 0; i < inputData.length; i++) {
+				for (var j = 0; j < inputData.length; j++) {
 					if (!isNaN(array[i][iter]) && !isNaN(array[iter][j])) {
 						tmpInt = parseInt(array[i][iter], 10) + parseInt(array[iter][j], 10);
 						if (tmpInt < array[i][j] || array[i][j] == "z") {
@@ -47,26 +47,20 @@ function run() {
 		console.log(array);
 		}
 
-		let a, b;
-		a = document.getElementById("a-input").value;
-		b = document.getElementById("b-input").value;
+		var result = "";
+		for (var i = 0; i < size; i++) {
+			for (var j = 0; j < size; j++) {
+				if (!isNaN(array[i][j])) {
+					result += "1 ";
+				}
+				else {
+					result += "0 ";
+				}
+			}
+			result += "\n";
+		}
 
-		if (a > size || b > size) {
-			alert("Bruh, у нас всего " + size + " элементов, надо бы поменьше")
-		}
-		else if (a < 1 || b < 1) {
-			alert("Bruh, у нас элементы считаются с первого, надо бы побольше")
-		}
-		else {
-			document.getElementById("a").innerHTML = a;
-			document.getElementById("b").innerHTML = b;
-			if (array[a - 1][b - 1] != "z") {
-				document.getElementById("result").innerHTML = array[a - 1][b - 1];
-			}
-			else {
-				document.getElementById("result").innerHTML = "нет дороги";
-			}
-		}		
+		document.getElementById("output-field").innerHTML = result;
 	}
 	else if (Vald == 1){
 		alert("В матрице могут быть только числа либо буква \"z\"!");
